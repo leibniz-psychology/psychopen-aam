a8b73482fa690a8e7a4fbba3c366f7bd285ce007
Ronny Bölter <ronny.boelter@outlook.com>
2020-10-15 13:51:44 +0200

    async rebuild for better page load

--- handler/AAMPluginHandler.inc.php
+++ handler/AAMPluginHandler.inc.php
@@ -30,23 +30,52 @@ class AAMPluginHandler extends Handler
 			if ($submission->getStatus() == self::STATUS_QUEUED
 				&& ($submission->getStageId() == self::WORKFLOW_STAGE_ID_EDITING
 					|| $submission->getStageId() == self::WORKFLOW_STAGE_ID_PRODUCTION)) {
-				if (isset($pubIdPrefix)) {
-					$paItem = $this->_searchPsychArchivesItemByIsVersionOf($pubIdPrefix.$submission->getId());
-					//$paItem = $this->_searchPsychArchivesItemByIsVersionOf('https://doi.org/10.5964/meth.2807');
-				}
-				if (isset($paItem) && is_array($paItem) && sizeof($paItem) > 0) {
-					$paItemLink = self::PA_BASE_URL.'/handle/'.$paItem[0]['handle'];
-				}
 				$aamItems[] = [
 					'title' => $submission->getTitle($submission->getLocale()),
 					'authors' => $submission->getAuthorString(),
-					'linkToPsychArchives' => isset($paItemLink) ? $paItemLink : null,
+					'preliminaryDOI' => $pubIdPrefix.$submission->getId(),
 				];
 			}
 		}
 		$templateMgr->assign('aamItems', $aamItems);
-
 		return $templateMgr->display($plugin->getTemplateResource('aamList.tpl'));
+	}
+
+	/**
+	 * @param $args
+	 * @param $request
+	 * @return mixed|null
+	 */
+	public function getPsychArchivesLink($args, $request)
+	{
+		if ($request->getUserVars() && sizeof($request->getUserVars()) > 0 && $request->getUserVars()['doi']) {
+			$relation = $request->getUserVars()['doi'];
+			try {
+				$curl = curl_init();
+				curl_setopt_array(
+					$curl,
+					array(
+						CURLOPT_URL => self::PA_BASE_URL."/rest/items/find-by-metadata-field",
+						CURLOPT_RETURNTRANSFER => true,
+						CURLOPT_HTTPHEADER => array('Accept: application/json', 'Content-Type: application/json'),
+						CURLOPT_POST => true,
+						CURLOPT_POSTFIELDS => '{"key": "dc.relation.isversionof","value": "'.$relation.'","language":null}',
+					)
+				);
+				$result = curl_exec($curl);
+				curl_close($curl);
+				$itm = json_decode($result, true);
+				if (isset($itm) && is_array($itm) && sizeof($itm) > 0) {
+					$paLink = self::PA_BASE_URL.'/handle/'.$itm[0]['handle'];
+
+					return new JSONMessage(true, ['paLink' => $paLink]);
+				}
+			} catch (Exception $e) {
+
+			}
+		}
+
+		return new JSONMessage(false);
 	}
 
 	/**
@@ -68,32 +97,4 @@ class AAMPluginHandler extends Handler
 		return $pubIdPrefix;
 	}
 
-
-	/**
-	 * @param $relation
-	 * @return mixed|null
-	 */
-	private function _searchPsychArchivesItemByIsVersionOf($relation)
-	{
-		try {
-			$curl = curl_init();
-			curl_setopt_array(
-				$curl,
-				array(
-					CURLOPT_URL => self::PA_BASE_URL."/rest/items/find-by-metadata-field",
-					CURLOPT_RETURNTRANSFER => true,
-					CURLOPT_HTTPHEADER => array('Accept: application/json', 'Content-Type: application/json'),
-					CURLOPT_POST => true,
-					CURLOPT_POSTFIELDS => '{"key": "dc.relation.isversionof","value": "'.$relation.'","language":null}',
-				)
-			);
-			$result = curl_exec($curl);
-			curl_close($curl);
-			$itm = json_decode($result, true);
-		} catch (Exception $e) {
-			$itm = null;
-		}
-
-		return $itm;
-	}
 }
--- templates/aamList.tpl
+++ templates/aamList.tpl
@@ -1,40 +1,41 @@
 {include file="frontend/components/header.tpl" pageTitleTranslated=$issueIdentification}
 
 <div class="page page_aam">
-	{include file="frontend/components/breadcrumbs.tpl" currentTitleKey="plugins.generic.aam.breadcrumb"}
+    {include file="frontend/components/breadcrumbs.tpl" currentTitleKey="plugins.generic.aam.breadcrumb"}
 	<h1>{translate key="plugins.generic.aam.headline"}</h1>
-	{if empty($aamItems)}
-		{include file="frontend/components/notification.tpl" type="warning" messageKey="plugins.generic.aam.help.no.content"}
-	{else}
+    {if empty($aamItems)}
+        {include file="frontend/components/notification.tpl" type="warning" messageKey="plugins.generic.aam.help.no.content"}
+    {else}
 		<div class="cmp_notification info">
-			{translate key="plugins.generic.aam.help.content" journalName=$displayPageHeaderTitle|escape}
+            {translate key="plugins.generic.aam.help.content" journalName=$displayPageHeaderTitle|escape}
 		</div>
 		<ul class="cmp_article_list articles obj_article_summary">
-			{foreach from=$aamItems item=aamItem}
+            {foreach from=$aamItems item=aamItem}
 				<li>
 					<div class="obj_article_summary">
 						<h3 class="title">
-							{$aamItem['title']|escape}
+                            {$aamItem['title']|escape}
 						</h3>
 						<div class="meta">
 							<div class="authors">
-								{$aamItem['authors']|escape}
+                                {$aamItem['authors']|escape}
 							</div>
 						</div>
 					</div>
-					{if isset($aamItem['linkToPsychArchives']) && !empty($aamItem['linkToPsychArchives'])}
-						<ul class="galleys_links">
+                    {if isset($aamItem['preliminaryDOI']) && !empty($aamItem['preliminaryDOI'])}
+						<ul class="galleys_links pa-link-query" style="display: none" data-url="{url page="aam" op="getPsychArchivesLink"}"
+						    data-doi="{$aamItem['preliminaryDOI']}">
 							<li>
-								<a class="obj_galley_link" href="{$aamItem['linkToPsychArchives']}" target="_blank" rel="noreferrer">
-									{translate key="plugins.generic.aam.link.pa"}
+								<a class="obj_galley_link" href="" target="_blank" rel="noreferrer">
+                                    {translate key="plugins.generic.aam.link.pa"}
 								</a>
 							</li>
 						</ul>
-					{/if}
+                    {/if}
 				</li>
-			{/foreach}
+            {/foreach}
 		</ul>
-	{/if}
-	{*{url page="aam"}*}
+    {/if}
+    {*{url page="aam"}*}
 </div>
 {include file="frontend/components/footer.tpl"}
